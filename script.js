920231d51666c21aa2b5618642a9dcd540d73383
Jonathan Mazin <jonathan.mazin@gmail.com>
2022-11-29 20:05:43 -0500

    add checkout button

--- script.js
+++ script.js
@@ -5,56 +5,66 @@
 // https://api.punkapi.com/v2/beers
 
 let total = 0;
-const tableBody = document.getElementById('table-body');
+const cart = [];
+
+const tableBody = document.getElementById("table-body");
+const size = document.getElementById("size");
+
+document.getElementById("check-out").addEventListener("click", () => {
+  let total = 0;
+  let items = "";
+  cart.forEach((item) => {
+    items += item.title + "\n";
+    total += item.price;
+  });
+  alert(
+    `Checkout: ${cart.length} items:\n${items}\nfor a total of ${total.toFixed(2)}.`
+  );
+});
 
 //document.getElementById("total").innerText = total;
 
-fetch("https://fakestoreapi.com/products").then((data) => {
+fetch("https://fakestoreapi.com/products")
+  .then((data) => {
     //console.log(data); //JSON format
     return data.json(); //converted to object
   })
-  .then((objectData) => {
-    const listOfRows = objectData.forEach((values) => {
+  .then((products) => {
+    products.forEach((product) => {
       const row = document.createElement("tr");
 
       const title = document.createElement("td");
-      title.innerText = values.title;
+      title.innerText = product.title;
       row.append(title);
 
       const desc = document.createElement("td");
-      desc.innerText = values.description;
+      desc.innerText = product.description;
       row.append(desc);
 
       const price = document.createElement("td");
-      price.innerText = values.price;
+      price.innerText = product.price;
       row.append(price);
 
       const category = document.createElement("td");
-      category.innerText = values.category;
+      category.innerText = product.category;
       row.append(category);
 
       const imageTd = document.createElement("td");
       const img = document.createElement("img");
       const button = document.createElement("button");
       button.textContent = "Add to cart";
-      img.src = values.image;
+      img.src = product.image;
       imageTd.append(img);
       imageTd.append(button);
 
       row.append(imageTd);
 
-      // button.addEventListener("click", () => {
-      //   items.push(values);
-      //   size.textContent = items.length;
-      //   total.textContent = items.reduce(
-      //     (total, item) => total + item.price,
-      //     0
-      //   );
-      // });
+      button.addEventListener("click", () => {
+        cart.push(product);
+        size.textContent = cart.length;
+      });
       tableBody.append(row);
-
     });
-
   })
   .catch((error) => {
     console.log(error);
